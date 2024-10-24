5c0728783c6d4ecd7838e005ddd0da25e36aa84d
Renan Georgio <43246444+RenanGeorgio@users.noreply.github.com>
2024-10-24 15:49:06 -0300

    Update index.ts

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,15 +1,22 @@
 import mongoose from "mongoose";
 import config from "../config/env";
 
+// Connect to MongoDB (make sure you include the replica set in the URI)
+// 'mongodb://localhost:27017/your-db?replicaSet=rs0'
+
 const url: string = config.app.database;
 
 (async () => {
-  await mongoose.connect(url).then(() => { 
+  await mongoose.connect(url, 
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,      
+    }
+  ).then(() => { 
     console.log("Database connected!") 
   }).catch((err: any) => { 
     console.log(err.message) 
   });
-
   mongoose.Promise = global.Promise;
 })();
 
